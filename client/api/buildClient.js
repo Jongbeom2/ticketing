ed418d1a887d47666caaf9511fdde35a028d4338
사용자 <n11334@naver.com>
2022-01-15 13:38:58 +0900

    test

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -2,8 +2,7 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.waldenhouse.co.kr/",
       headers: req.headers,
     });
   } else {
